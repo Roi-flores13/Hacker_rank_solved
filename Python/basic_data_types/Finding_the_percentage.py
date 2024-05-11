54d22f2966262a7f5387d244efab7ffa27375483
Roi-flores13 <roi.flores@iteso.mx>
2024-05-10 19:45:53 -0600

    find the percentage

--- Python/basic_data_types/Finding_the_percentage.py
+++ Python/basic_data_types/Finding_the_percentage.py
@@ -6,3 +6,12 @@ if __name__ == '__main__':
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name = input()
+    
+    average = round(sum(student_marks[query_name]) / len(student_marks[query_name]), 3)
+    average = str(average)
+    
+    #This is not necessary, this part of the code is only implemented for output requirements for hacker rank.
+    #The average is correctly computed, this part only adds an extra zero if the float only has 1 decimal
+    if len(average) == 4:
+        average = average + "0"   
+    print(average)
