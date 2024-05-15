3c036b2cafe8542efddd117746da76b0c6c3b4dd
Roi-flores13 <roi.flores@iteso.mx>
2024-05-15 14:24:02 -0600

    lists solution

--- Python/basic_data_types/lists.py
+++ Python/basic_data_types/lists.py
@@ -1,2 +1,30 @@
 if __name__ == '__main__':
     N = int(input())
+    initial = []
+    
+    for command in range(N):
+        operation = input().lower().strip().split(" ")
+        
+        if len(operation) == 1:
+            
+            if operation[0] == "print":
+                print(initial)
+                
+            elif operation[0] == "pop":
+                initial.pop()
+                
+            elif operation[0] == "reverse":
+                initial.reverse()
+                
+            elif operation[0] == "sort":
+                initial.sort()
+                
+        elif len(operation) == 2:
+            
+            if operation[0] == "remove":
+                initial.remove(int(operation[1]))
+            elif operation[0] == "append":
+                initial.append(int(operation[1]))
+                
+        elif len(operation) == 3:
+            initial.insert(int(operation[1]), int(operation[2]))
