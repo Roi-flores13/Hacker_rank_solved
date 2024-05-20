2827b6ddf514eab9ec9f63c3bc86f030962193ed
Roi-flores13 <roi.flores@iteso.mx>
2024-05-20 14:15:09 -0600

    polar coordinates

--- Python/Math/polar_coordinates.py
+++ Python/Math/polar_coordinates.py
@@ -1,49 +1,7 @@
-import cmath as cm
-z = input().strip()
-if "+" in z:
-    
-    if "j" in z.split("+")[1] and len(z) != 3:
-        real = int(z.split("+")[0])
-        imaginary = int(z.split("+")[1][0])
-        
-    elif "j" in z.split("+")[0] and len(z) != 3:
-        real = int(z.split("+")[1])
-        imaginary = int(z.split("+")[0][0])
-        
-    else:
-        real = int(z.split("+")[0])
-        imaginary = 1
-        
-elif "-" in z: 
-    if z.count("-") == 1:
-        
-        if "j" in z.split("-")[1]:
-            real = int(z.split("-")[0])
-            imaginary = int(z.split("-")[1][0])
-            
-        else:
-            real = int(z.split("-")[1])
-            imaginary = int(z.split("-")[0][0])
-            
-    else:
-        if "j" in z.split("-")[2]:
-            real = int(z.split("-")[1])
-            imaginary = int(z.split("-")[2].split("j")[0])
-        
-        else:
-            if "j" in z.split("-")[1]:
-                real = int(z.split("-")[2])
-                imaginary = int(z.split("-")[1].split("j")[0])
-            
+import cmath
 
-else:
-    if "j" in z:
-        real = 0
-        imaginary = int(z.split("j")[0])
-    else:
-        real = int(z)
-        imaginary = 0
+z = complex(input())
+mod, arg = cmath.polar(z)
 
-
-print(abs(complex(real, imaginary)))
-print(cm.phase(complex(real, imaginary)))
+print(mod)
+print(arg)
